73b1fcfaa4c799f1c7b83045e737c4af081daf23
elyas <elyashedayat10@gmail.com>
2022-07-29 08:22:09 -0700

    change default permission

--- irandot/settings.py
+++ irandot/settings.py
@@ -145,4 +145,8 @@ AUTH_USER_MODEL = 'accounts.User'
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
-    ]}
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
