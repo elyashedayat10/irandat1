72953657f4482820c61a5cadad16207e19665f1e
ilgharhedayat <ilgharhedayatqarahqayehloo@gmail.com>
2022-07-30 12:43:43 -0700

    v1

--- config/api/views.py
+++ config/api/views.py
@@ -1,12 +1,16 @@
-from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.generics import GenericAPIView, CreateAPIView
 from .serializers import GuideSerializer
 from ..models import Guide
 from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework import status
 
-
-class GuideApiView(ListAPIView):
-    queryset = Guide.load()
+class GuideApiView(GenericAPIView):
     serializer_class = GuideSerializer
+
+    def get(self,request,*args,**kwargs):
+        serializer=self.serializer_class(Guide.load()).data
+        return Response(data=serializer,status=status.HTTP_200_OK)
 
 class GuideCreateApiView(CreateAPIView):
     queryset = Guide.load()
