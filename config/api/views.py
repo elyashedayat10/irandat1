c9e730754dbd089212a0ec5d8ea32724ae25e71b
ilgharhedayat <ilgharhedayatqarahqayehloo@gmail.com>
2022-07-30 12:49:05 -0700

    add views to guid

--- config/api/views.py
+++ config/api/views.py
@@ -5,14 +5,16 @@ from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from rest_framework import status
 
+
 class GuideApiView(GenericAPIView):
     serializer_class = GuideSerializer
 
-    def get(self,request,*args,**kwargs):
-        serializer=self.serializer_class(Guide.load()).data
-        return Response(data=serializer,status=status.HTTP_200_OK)
+    def get(self, request, *args, **kwargs):
+        serializer = self.serializer_class(Guide.load()).data
+        return Response(data=serializer, status=status.HTTP_200_OK)
+
 
 class GuideCreateApiView(CreateAPIView):
-    queryset = Guide.load()
+    queryset = Guide.objects.all()
     serializer_class = GuideSerializer
     permission_classes = [IsAdminUser, ]
