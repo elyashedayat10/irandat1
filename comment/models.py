fa1a15dfc4b3fe657a0337b3f957d03ef266ab17
ilgharhedayat <ilgharhedayatqarahqayehloo@gmail.com>
2022-07-31 14:08:14 -0700

    adding null =True to comment model parent field

--- comment/models.py
+++ comment/models.py
@@ -11,7 +11,7 @@ class Comment(TimeStampedModel):
     legal_article = models.ForeignKey('legalarticle.LegalArticle', on_delete=models.CASCADE, related_name='comments')
     user = models.ForeignKey(user, on_delete=models.CASCADE, related_name='user_comments')
     content = models.TextField()
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='children')
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='children',null=True,blank=True)
     confirmed = models.BooleanField(default=False)
 
     def __str__(self):
