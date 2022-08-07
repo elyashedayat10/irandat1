0fca0d40eebe51c562bf0a62de913425fa168607
ilgharhedayat <ilgharhedayatqarahqayehloo@gmail.com>
2022-08-07 02:24:26 -0700

    fix url conf

--- tickets/api/urls.py
+++ tickets/api/urls.py
@@ -8,7 +8,7 @@ app_name = 'tickett'
 urlpatterns = [
     path('', UserTicketApiView.as_view()),
     path('list/', TickerListApiView.as_view()),
-    path('<int:ticket_id>/', TicketDetailApiView.as_view()),
+    path('<int:pk>/', TicketDetailApiView.as_view()),
     path('close/<int:ticket_id>/', TicketCloseApiView.as_view()),
     path('create/', TicketCreateApiView.as_view()),
     path('answer/<int:ticket_id>/', AnswerCreateApiView.as_view()),
