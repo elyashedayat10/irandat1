79e1e43f6277d116a060b74e8198fe6c278a355d
ilgharhedayat <ilgharhedayatqarahqayehloo@gmail.com>
2022-08-20 12:23:16 -0700

    comment config part

--- config/api/urls.py
+++ config/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import GuideApiView, GuideCreateApiView, SettingCreateApiView, SettingApiView
+from . import views
+    # GuideApiView, GuideCreateApiView, SettingCreateApiView, SettingApiView
 
 urlpatterns = [
-    path('', GuideApiView.as_view()),
-    path('create/', GuideCreateApiView.as_view()),
-    path('setting_create/', SettingCreateApiView.as_view()),
-    path('setting/', SettingApiView.as_view()),
+    # path('', GuideApiView.as_view()),
+    # path('create/', GuideCreateApiView.as_view()),
+    # path('setting_create/', SettingCreateApiView.as_view()),
+    # path('setting/', SettingApiView.as_view()),
 ]
--- config/api/views.py
+++ config/api/views.py
@@ -6,29 +6,29 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
-class GuideApiView(GenericAPIView):
-    serializer_class = GuideSerializer
-
-    def get(self, request, *args, **kwargs):
-        serializer = self.serializer_class(Guide.load()).data
-        return Response(data=serializer, status=status.HTTP_200_OK)
-
-
-class GuideCreateApiView(CreateAPIView):
-    queryset = Guide.load()
-    serializer_class = GuideSerializer
-    permission_classes = [IsAdminUser, ]
+# class GuideApiView(GenericAPIView):
+#     serializer_class = GuideSerializer
+#
+#     def get(self, request, *args, **kwargs):
+#         serializer = self.serializer_class(Guide.load()).data
+#         return Response(data=serializer, status=status.HTTP_200_OK)
 
 
-class SettingCreateApiView(CreateAPIView):
-    queryset = Setting.load()
-    serializer_class = SettingSerializer
-    permission_classes = [IsAdminUser, ]
+# class GuideCreateApiView(CreateAPIView):
+#     queryset = Guide.load()
+#     serializer_class = GuideSerializer
+#     permission_classes = [IsAdminUser, ]
 
 
-class SettingApiView(GenericAPIView):
-    serializer_class = SettingSerializer
-
-    def get(self, request, *args, **kwargs):
-        serializer = self.serializer_class(Setting.load()).data
-        return Response(data=serializer, status=status.HTTP_200_OK)
+# class SettingCreateApiView(CreateAPIView):
+#     queryset = Setting.load()
+#     serializer_class = SettingSerializer
+#     permission_classes = [IsAdminUser, ]
+#
+#
+# class SettingApiView(GenericAPIView):
+#     serializer_class = SettingSerializer
+#
+#     def get(self, request, *args, **kwargs):
+#         serializer = self.serializer_class(Setting.load()).data
+#         return Response(data=serializer, status=status.HTTP_200_OK)
