48bca55652252377fe5af9474a4a6426aabebed8
ilgharhedayat <ilgharhedayatqarahqayehloo@gmail.com>
2022-08-25 03:35:17 -0700

    fix bug in owener permission

--- comment/api/permissions.py
+++ comment/api/permissions.py
@@ -3,5 +3,5 @@ from rest_framework import permissions
 
 class OwnerPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return request.user == obj.user or request.usesr.is_admin
+        return request.user == obj.user or request.user.is_admin
 
