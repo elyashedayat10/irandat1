53d323a5064f70854381d2e34b760bbb65a8d716
ilgharhedayat <ilgharhedayatqarahqayehloo@gmail.com>
2022-08-26 15:55:21 -0700

    register notif models in admin

--- config/admin.py
+++ config/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Setting
+from .models import Setting, Notification
 
 # Register your models here.
 admin.site.register(Setting)
+admin.site.register(Notification)
