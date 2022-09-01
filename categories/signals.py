ebd909004dbadacecea5c1755b021627740482d1
ilgharhedayat <ilgharhedayatqarahqayehloo@gmail.com>
2022-08-31 17:09:47 -0700

    test for fixing bug in signals

--- categories/signals.py
+++ categories/signals.py
@@ -1,9 +1,9 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_delete
 from django.db.models import F
 from django.dispatch import receiver
 from .models import Category
 
 
-@receiver(post_save, sender=Category)
+@receiver(post_delete, sender=Category)
 def update_category_order(sender, **kwargs):
     Category.objects.filter(order__gt=kwargs['instance'].order).update(F('order') - 1)
