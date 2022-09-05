6649ac286c37e9497b0d2ebac78f12ce13bd8588
ilgharhedayat <ilgharhedayatqarahqayehloo@gmail.com>
2022-09-05 10:40:49 -0700

    test

--- categories/signals.py
+++ categories/signals.py
@@ -8,6 +8,7 @@ from .models import Category
 def update_category_order(sender, **kwargs):
     category_obj = Category.objects.get(id=kwargs['instance'].id)
     if category_obj.parent:
-        Category.objects.filter(parent=category_obj.parent).update(order=F('order') - 1)
+        Category.objects.filter(parent=category_obj.parent).filter(order__gt=kwargs['instance'].order).update(
+            order=F('order') - 1)
     else:
-        Category.objects.filter(parent=None).update(order=F('order') - 1)
+        Category.objects.filter(parent=None).filter(order__gt=kwargs['instance'].order).fupdate(order=F('order') - 1)
