ba977475bdb7fb99792804f802bbe95c9e2e74bf
ilgharhedayat <ilgharhedayatqarahqayehloo@gmail.com>
2022-09-08 17:15:40 -0700

    commit category signal

--- categories/signals.py
+++ categories/signals.py
@@ -7,16 +7,9 @@ from .models import Category
 @receiver(pre_delete, sender=Category)
 def update_category_order(sender, **kwargs):
     category_obj = Category.objects.get(id=kwargs['instance'].id)
-    print(
-        "eluas"
-    )
-    if category_obj.parent:
-        test = Category.objects.filter(parent=category_obj.parent).filter(order__gt=kwargs['instance'].order).update(
-            order=F('order') - 1)
-        print("ilghar")
-        print(test)
-    else:
-        test = Category.objects.filter(parent=None).filter(order__gt=kwargs['instance'].order).update(
-            order=F('order') - 1)
-        print("reza")
-        print(test)
+    # if category_obj.parent:
+    #     Category.objects.filter(parent=category_obj.parent).filter(order__gt=kwargs['instance'].order).update(
+    #         order=F('order') - 1)
+    # else:
+    Category.objects.filter(parent=None).filter(order__gt=kwargs['instance'].order).update(
+        order=F('order') - 1)
