e28fba2de9fb44f8696d556384341c7410b18938
ilgharhedayat <ilgharhedayatqarahqayehloo@gmail.com>
2022-09-11 05:08:55 -0700

    change database

--- config/api/views.py
+++ config/api/views.py
@@ -13,36 +13,36 @@ from ..models import Guide, Notification, Setting
 from .serializers import GuideSerializer, NotificationSerializer, SettingSerializer
 
 
-class GuideApiView(GenericAPIView):
-    serializer_class = GuideSerializer
-
-    def get(self, request, *args, **kwargs):
-        serializer = self.serializer_class(Guide.load()).data
-        return Response(data=serializer, status=status.HTTP_200_OK)
-
-
-class GuideCreateApiView(CreateAPIView):
-    queryset = Guide.load()
-    serializer_class = GuideSerializer
-    permission_classes = [
-        IsAdminUser,
-    ]
+# class GuideApiView(GenericAPIView):
+#     serializer_class = GuideSerializer
+#
+#     def get(self, request, *args, **kwargs):
+#         serializer = self.serializer_class(Guide.load()).data
+#         return Response(data=serializer, status=status.HTTP_200_OK)
 
 
-class SettingCreateApiView(CreateAPIView):
-    queryset = Setting.load()
-    serializer_class = SettingSerializer
-    permission_classes = [
-        IsAdminUser,
-    ]
+# class GuideCreateApiView(CreateAPIView):
+#     # queryset = Guide.load()
+#     serializer_class = GuideSerializer
+#     permission_classes = [
+#         IsAdminUser,
+#     ]
 
 
-class SettingApiView(GenericAPIView):
-    serializer_class = SettingSerializer
+# class SettingCreateApiView(CreateAPIView):
+#     queryset = Setting.load()
+#     serializer_class = SettingSerializer
+#     permission_classes = [
+#         IsAdminUser,
+#     ]
 
-    def get(self, request, *args, **kwargs):
-        serializer = self.serializer_class(Setting.load()).data
-        return Response(data=serializer, status=status.HTTP_200_OK)
+
+# class SettingApiView(GenericAPIView):
+#     serializer_class = SettingSerializer
+#
+#     def get(self, request, *args, **kwargs):
+#         serializer = self.serializer_class(Setting.load()).data
+#         return Response(data=serializer, status=status.HTTP_200_OK)
 
 
 class NotificationListApiView(ListAPIView):
