de485d4a0e358dc7e6634f94731a30f1ef14b0bc
ilgharhedayat <ilgharhedayatqarahqayehloo@gmail.com>
2022-09-13 12:58:01 -0700

    add permission to article models

--- accounts/admin.py
+++ accounts/admin.py
@@ -29,7 +29,7 @@ class UserAdmin(BaseAdmin):
     fieldsets = (
         (
             "Main",
-            {"fields": ("phone_number", "fcm_token")},
+            {"fields": ("phone_number", )},
         ),
         (
             "Permissions",
